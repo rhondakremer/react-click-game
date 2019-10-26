25606ce4955df07287c0775fb8560f408759d76b
Rhonda Kremer <rhondakremer@gmail.com>
2019-10-26 09:58:28 -0400

    Updates

---
+++ precache-manifest.fd188577a03694e96c6da7144878fd4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d80a7858bf4ab69457b57305698b736",
+    "url": "/react-click-game/index.html"
+  },
+  {
+    "revision": "712a79223141fcc4f15e",
+    "url": "/react-click-game/static/css/main.47ffb5d8.chunk.css"
+  },
+  {
+    "revision": "6d1f1092f04a3a59c72d",
+    "url": "/react-click-game/static/js/2.816fa133.chunk.js"
+  },
+  {
+    "revision": "712a79223141fcc4f15e",
+    "url": "/react-click-game/static/js/main.d5a628de.chunk.js"
+  },
+  {
+    "revision": "4b4487c97bfae3b2a0c2",
+    "url": "/react-click-game/static/js/runtime-main.66a54b6e.js"
+  }
+]);
