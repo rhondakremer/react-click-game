f453d96d82d7a39b5734d493fd86e2f964e05358
Rhonda Kremer <rhondakremer@gmail.com>
2019-11-04 14:59:08 -0500

    added bootstrap

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,8 +3,8 @@ import "./style.css";
 
 function Header(props) {
   return  <div>
-      <h1 className="header-title">Puppy Click Game!</h1>
-      <h5 className>Click a puppy to start. But don't click any puppy more than once!</h5>
+      <h1 className="header-title col-12">Puppy Click Game!</h1>
+      <h5>Click a puppy to start. But don't click any puppy more than once!</h5>
   <p>Score: {props.score} High Score: {props.highScore}</p>
   </div>
 }
